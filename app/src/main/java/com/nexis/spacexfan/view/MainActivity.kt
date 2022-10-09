55b7d3daa08f5cad9e51f3233253efef055349a4
Nexis <mustafa.salih.eybek.07@gmail.com>
2022-10-09 18:35:41 +0300

    Initial commit

---
+++ app/src/main/java/com/nexis/spacexfan/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nexis.spacexfan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nexis.spacexfan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
